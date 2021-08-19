4db204cf203246d5c37ff2b4467b24f5e5c2e6f7
David Pérez <knightbcn@gmail.com>
2021-08-19 13:55:28 +0200

    autocomplete

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,29 +6,31 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
 // Módulos
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroesRoutingModule } from './heroes-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
+import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 import { ImagenPipe } from './pipes/imagen.pipe';
+import { MaterialModule } from '../material/material.module';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AgregarComponent,
     BuscarComponent,
     HeroeComponent,
-    HomeComponent,
-    ListadoComponent,
     HeroeTarjetaComponent,
+    HomeComponent,
     ImagenPipe,
+    ListadoComponent,
   ],
   imports: [
     CommonModule,
     HeroesRoutingModule,
     FlexLayoutModule,
+    FormsModule,
     MaterialModule,
   ],
 })
-export class HeroesModule {}
+export class HeroesModule { }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,20 +1,26 @@
 // Componentes
 
 // Módulos
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card'
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
 @NgModule({
   exports: [
+    MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatGridListModule,
     MatListModule,
     MatProgressSpinnerModule,
